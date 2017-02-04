5010a8cc6236014d85f164f910601dd01b2b9d12
Noah Schutte <noahschutte@gmail.com>
2017-02-03 20:42:26 -0700

    add validation on word to include only one word consisting of all letters

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,9 @@
 class Search < ApplicationRecord
   validates_presence_of :word, :message => "must be entered."
+  validates_format_of :word, :with => /\A[a-z]+\Z/i, :on => :create, :message => "must only use letters."
+  validates :word, length: {
+    maximum: 1,
+    tokenizer: lambda { |str| str.split(/\s+/) },
+    too_long: "can only be one word. Duh..."
+  }
 end
